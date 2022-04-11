dc57a0340be92220bb30a61b9f0260b77eccf1ec
Daniil Silin <daniil_silin@mail.ru>
2022-04-11 16:01:40 +0100

    Added meta

--- components/layouts/main.tsx
+++ components/layouts/main.tsx
@@ -9,7 +9,16 @@ const Main: ({children, router}: { children: any; router: any }) => JSX.Element 
       <Box as={"main"} pb={8}>
           <Head>
               <meta name={"viewport"} content={"width=device-width, initial-scale=1"}/>
-              <title>Daniil Silin - Main</title>
+
+              <meta name="description" content="Daniil Silin website" />
+              <meta name="author" content="Daniil Silin" />
+              <link rel="apple-touch-icon" href="icon.png" />
+              <link rel="shortcut icon" href="/icon.png" type="image/x-icon" />
+
+              <meta property="og:site_name" content="Daniil Silin website" />
+              <meta property="og:type" content="website" />
+
+              <title>Daniil Silin</title>
           </Head>
 
           <NavBar path={router.asPath}/>
